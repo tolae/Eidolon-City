4768b0e0efc1aa8354735f9a890282eec0033f06
Ethan Tola <ethanltola@gmail.com>
2018-09-25 22:57:36 -0400

    Began player movement

---
+++ Assets/Scripts/MyCharacterController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MyCharacterController : MonoBehaviour {
+    /*General Fields*/
+    private float jumpTime = 3f; //Seconds
+    private float jumpCooldown = 1f; //Seconds
+    private bool isJumping = false;
+    private Rigidbody2D rigidbody2D;
+
+    private Vector2 reference_velocity = Vector2.zero;
+    [Range(0, 0.3f)] [SerializeField] private float movement_smoothing = 0.05f;
+    /*Events*/
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
+    public BoolEvent onJumpEvent; //For when the player jumps up from the land
+
+    private void Awake() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        if (onJumpEvent == null)
+            onJumpEvent = new BoolEvent();
+    }
+
+    public IEnumerator Move(Vector2 move, bool jump) {
+        /* If not jumping, move using the move command.       *
+         * If jumping, move at the constant velocity when the *
+         * player decided to jump.                            */
+        if (!isJumping) {
+            rigidbody2D.velocity = Vector2.SmoothDamp(
+                rigidbody2D.velocity,
+                move,
+                ref reference_velocity,
+                movement_smoothing);
+        }
+        /*Make sure the Player cannot constantly jump*/
+        if (jump && !isJumping) {
+            isJumping = true;
+            onJumpEvent.Invoke(isJumping);
+            yield return new WaitForSeconds(jumpTime);
+            isJumping = false;
+            onJumpEvent.Invoke(isJumping);
+            yield return new WaitForSeconds(jumpCooldown);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     /*Player Fields*/
-    float health; //The number of hitpoints a player can take before death
-    float speed;  //How fast the player moves
+    [SerializeField] private float health = 5; //The number of hitpoints a player can take before death
+    [SerializeField] private float speed = 10;  //How fast the player moves
+    [SerializeField] private float jumpSpeed = 15; //How fast the player moves while jumping
+    private Vector2 movement; //The directional of where the player wants to move
+    private bool jump; //If the character hit to jump or not
+
+    public MyCharacterController controller;
+    public Animator animator;
 
 	// Use this for initialization
 	void Start () {
         health = 5;
         speed = 10;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void Update() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        jump = Input.GetKeyDown(KeyCode.Space);
+
+        animator.SetFloat("Speed", movement.magnitude);
+
+        movement *= speed;
+    }
+
+    private void FixedUpdate() {
+        StartCoroutine(controller.Move(movement, jump));
+    }
+
+    public void OnJump(bool isJumping) {
+        animator.SetBool("Jumping", isJumping);
+    }
 }
