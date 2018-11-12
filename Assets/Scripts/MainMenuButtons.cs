98fbdb389bf21a19ed09d6eee1a2a614170d7b46
Ethan Tola <ethanltola@gmail.com>
2018-11-11 20:46:16 -0500

    Made a simple menu UI

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+
+	public void OnStartClick() {
+        SceneManager.LoadScene("DreamArena_Test");
+    }
+
+    public void OnExitClick() {
+        Application.Quit();
+    }
+
+    public void OnStoryClick() {
+        //SceneManager.LoadScene("MainStory");
+    }
+}
